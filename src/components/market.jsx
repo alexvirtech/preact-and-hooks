f32ca14f6d6990373b6c020a63e505178399a233
alexvirtech <alex@vir-tec.net>
2024-07-06 19:38:02 +0300

    Sat 07-06-24

--- src/components/market.jsx
+++ src/components/market.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "preact/hooks"
+import CoinRow from "./coinRow"
 
 export default function Market() {
     const [data, setData] = useState(null)
@@ -16,16 +17,14 @@ export default function Market() {
         <div>
             <h1 class="text-2xl font-bold pb-4">Cryptocurrencies</h1>
             {data &&
-                Object.entries(data).map(([key, value]) => {
-                    return (
-                        <div class="border-t border-gray-200 py-2 grid grid-cols-12">
-                            <div class="col-span-1"><img src={value.image} class="max-h-8"/></div>
-                            <div class="col-span-1">{key}</div>
-                            <div class="col-span-2">{value.name}</div>
-                            <div class="col-span-8 text-sm">{value.summary}</div>
-                        </div>
-                    )
-                })}
+                Object.entries(data).map(([key, value], index) => (
+                    <CoinRow
+                        symbol={key}
+                        coin={value}
+                        index={index}
+                        key={index}
+                    />
+                ))}
         </div>
     )
 }
